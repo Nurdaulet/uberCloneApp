9bf1cf1f58c26dd08f3e5f7e427390abfd7263c6
nurdaulet <nurdaulet1312@gmail.com>
2021-02-18 08:03:06 +0400

    initial

---
+++ src/screens/DestinationSearch/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+//import styles from './styles';
+
+
+const DestinationSearch = (props) => {
+    return (
+        <View>
+            <Text>
+                notjustcomp
+            </Text>
+        </View>
+    );
+};
+
+export default DestinationSearch;
